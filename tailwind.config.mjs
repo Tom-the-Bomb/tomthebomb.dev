b24b624d00cd5fe792b2754e6d864c5c9c10cb8d
Tom-the-Bomb <thello522@gmail.com>
2024-08-30 14:40:20 -0400

    main page

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'sans': ['Noto Sans', ...defaultTheme.fontFamily.sans],
+				'mono': ['Jetbrains Mono', ...defaultTheme.fontFamily.mono],
+			}
+		},
+	},
+	plugins: [],
+}
