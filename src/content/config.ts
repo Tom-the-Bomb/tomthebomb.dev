019fb07c2c0eefacdf853094615cb076f6581e50
Tom-the-Bomb <thello522@gmail.com>
2024-09-10 00:09:09 -0400

    wtf

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,11 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content';
 
 export const albums = defineCollection({
-    type: "data",
+    type: 'data',
     schema: ({ image }) =>
         z.object({
             title: z.string(),
+            short_description: z.string().optional(),
             description: z.string().optional(),
             cover: image(),
         }),
