bd1fe01127a56e282679224f394476459f5d532d
Tom-the-Bomb <77308095+Tom-the-Bomb@users.noreply.github.com>
2024-09-16 14:56:02 -0400

    stripped GPS info

--- process_images.py
+++ process_images.py
@@ -3,6 +3,7 @@ from os import remove
 from pathlib import Path
 
 from PIL import Image
+from PIL.ExifTags import IFD
 from pillow_heif import register_heif_opener
 
 def convert_heif(directory: str, *, quality: int = 100) -> None:
@@ -18,6 +19,21 @@ def convert_heif(directory: str, *, quality: int = 100) -> None:
             )
         print(f'Converted [{file}] from HEIC to JPEG successfully.')
         remove(file)
+
+    for file in Path(directory).glob('**/*.jpg'):
+        with Image.open(file) as img:
+            exif = img.getexif()
+
+            if exif.get(IFD.GPSInfo):
+                del exif[IFD.GPSInfo]
+
+                img.save(
+                    file,
+                    format='JPEG',
+                    quality=quality,
+                    exif=exif,
+                )
+                print(f'Successfully removed GPS metadata from [{file}]')
 
 if __name__ == '__main__':
     convert_heif('./src/content/albums/')
