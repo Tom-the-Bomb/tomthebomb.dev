04cabc3f864871cc2eac5671103472263f180661
Tom-the-Bomb <77308095+Tom-the-Bomb@users.noreply.github.com>
2026-02-08 14:43:38 -0500

    implemented SEO functionalities + fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [icon()],
+    site: "https://tomthebomb.dev",
+    integrations: [icon(), sitemap()],
     prefetch: {
         defaultStrategy: "viewport",
     },
